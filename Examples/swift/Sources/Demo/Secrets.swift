c41eaa6dd5ed2fa34bcacdfd80cbf076fc66f42d
Michael Henry Pantaleon <me@iamkel.net>
2023-03-22 21:33:46 +1100

    add more test case

--- Examples/swift/Sources/Demo/Secrets.swift
+++ Examples/swift/Sources/Demo/Secrets.swift
@@ -1,3 +1,4 @@
 enum Secrets {
   static let apiKey = "${X_API_KEY}"
+  static let otherApiKey = "${OTHER_API_KEY}"
 }
--- Examples/swift/Tests/DemoTests/DemoTests.swift
+++ Examples/swift/Tests/DemoTests/DemoTests.swift
@@ -5,4 +5,8 @@ final class DemoTests: XCTestCase {
     func testDeobfuscation() throws {
         XCTAssertEqual(Secrets.apiKey, "Some secret key")
     }
+
+    func testDeobfuscationWithSymbols() throws {
+        XCTAssertEqual(Secrets.otherApiKey, "j3kj3,,33rz,m234434443flll787483W22345333@32swift testswift test@@@&&")
+    }
 }
